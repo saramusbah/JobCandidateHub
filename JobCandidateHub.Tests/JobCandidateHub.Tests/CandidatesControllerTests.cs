64fe05982ad6009c8e26e4b6ae19d3f704e26c1d
Sara Nasser <s.nasser@smartbooqing.nl>
2024-07-01 15:59:05 +0530

    Fix Tests

--- JobCandidateHub.Tests/JobCandidateHub.Tests/CandidatesControllerTests.cs
+++ JobCandidateHub.Tests/JobCandidateHub.Tests/CandidatesControllerTests.cs
@@ -28,7 +28,7 @@ namespace JobCandidateHub.Tests
         [Fact]
         public async Task AddCandidate_ShouldReturnOK_WhenCandidateIsValid()
         {
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<OkObjectResult>(result);
         }
 
@@ -36,7 +36,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WhenEmailIsNotValid()
         {
             _candidateInputModel.Email = "notValidEmail";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
@@ -44,7 +44,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WhenFreeTextIsEmpty()
         {
             _candidateInputModel.FreeTextComment = "";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
@@ -52,7 +52,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WhenFirstNameIsEmpty()
         {
             _candidateInputModel.FirstName = "";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
@@ -60,7 +60,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WhenLastNameIsEmpty()
         {
             _candidateInputModel.LastName = "";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
@@ -68,7 +68,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WhenEmailIsEmpty()
         {
             _candidateInputModel.Email = "";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
@@ -76,7 +76,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WhenFirstNameIsBiggerThan50()
         {
             _candidateInputModel.FirstName = "Aequeosalinocalcalinoceraceoaluminosocupreovitriolic";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
@@ -84,7 +84,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WhenLastNameIsBiggerThan50()
         {
             _candidateInputModel.LastName = "Aequeosalinocalcalinoceraceoaluminosocupreovitriolic";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
@@ -92,7 +92,7 @@ namespace JobCandidateHub.Tests
         public async Task AddCandidate_ShouldReturnBadRequest_WheFreeTextIsBiggerThan500()
         {
             _candidateInputModel.FreeTextComment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula non odio suscipit placerat. Nulla facilisi. Donec nec mi in felis eleifend luctus.  Curabitur aliquam felis ac neque consectetur, eu vehicula urna dictum. Maecenas tempus posuere justo, vel tincidunt libero vehicula et. Proin fermentum imperdiet urna sit amet lacinia. Duis at rutrum leo. Ut eget tellus quis metus lobortis feugiat. Suspendisse potenti. Integer quis augue id felis commodo ultricies. Mauris in mi ut risus eleifend pretium at at ipsum. Nullam eleifend turpis sed justo molestie, id hendrerit libero mollis.";
-            var result = await _candidatesController.Add(_candidateInputModel);
+            var result = await _candidatesController.AddOrUpdate(_candidateInputModel);
             Assert.IsType<BadRequestObjectResult>(result);
         }
     }
